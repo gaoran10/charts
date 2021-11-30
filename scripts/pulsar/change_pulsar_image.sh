59d1678a02f6e5e69e0a5be58c9f75cd9c267e07
gaoran10 <gaoran_10@126.com>
2021-12-01 00:22:51 +0800

    fix

--- scripts/pulsar/change_pulsar_image.sh
+++ scripts/pulsar/change_pulsar_image.sh
@@ -29,4 +29,4 @@ echo "image_version: $image_version"
 
 rm -rf charts/pulsar-custom
 cp -r charts/pulsar-2.8.0.8 charts/pulsar-custom
-sed -i 's/2.8.0.8/2.9.0-candidate-4/g' charts/pulsar-custom/values.yaml
+sed -i "s/2.8.0.8/$image_version/g" charts/pulsar-custom/values.yaml
